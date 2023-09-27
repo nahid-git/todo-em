51a7b2197445ec7b240f2b659d48bd604a73ac8e
nahid-git <nahidcse.pub@gmail.com>
2023-09-27 20:00:51 +0600

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import {useState} from "react";
 import axios from "axios";
+import swal from 'sweetalert';
+
 
 function App() {
     let [title, setTitle] = useState("");
     let [description, setDescription] = useState("");
     let [status, setStatus] = useState("");
     let [date, setDate] = useState("");
-    let postResponseMessage;
     let handleSubmit = e => {
         e.preventDefault();
 
@@ -20,30 +21,56 @@ function App() {
         axios.post(`${import.meta.env.VITE_API_KEY}/todo`, {userObject})
 
             .then((res) => {
-                console.log(res.data.message)
-                postResponseMessage = res.data.message;
+                swal({
+                    title: "Success!",
+                    text: res.data.message,
+                    icon: "success",
+                    button: "OK",
+                })
             })
             .catch((error) => {
                 console.log(error)
             });
     }
-
     return (
         <div className='container'>
             <div>
-                <h3>hello</h3>
                 <form action="" onSubmit={handleSubmit}>
                     <label>Title </label>
-                    <input type="text" name="title" onChange={e => setTitle(e.target.value)}/>
+                    <input
+                        type="text"
+                        name="title"
+                        required
+                        onChange={e => setTitle(e.target.value)}
+                    />
                     <br/><br/>
                     <label>Description </label>
-                    <input type="text" name="description" onChange={e => setDescription(e.target.value)}/>
+                    <input
+                        type="text"
+                        name="description"
+                        required
+                        onChange={e => setDescription(e.target.value)}
+                    />
                     <br/><br/>
                     <label>Status </label>
-                    <input type="radio" value="active" name="status" onChange={e => setStatus(e.target.value)}/> Active
-                    <input type="radio" value="Inactive" name="status" onChange={e => setStatus(e.target.value)}/> Inactive
+                    <input
+                        type="radio"
+                        value="active"
+                        name="status"
+                        onChange={e => setStatus(e.target.value)}
+                    /> Active
+                    <input
+                        type="radio"
+                        value="Inactive"
+                        name="status"
+                        onChange={e => setStatus(e.target.value)}
+                    /> Inactive
                     <br/><br/>
-                    <input type="date"  name="date" onChange={e => setDate(e.target.value)}/>
+                    <input
+                        type="date"
+                        name="date"
+                        onChange={e => setDate(e.target.value)}
+                    />
                     <br/><br/>
                     <input type="submit"/>
                 </form>
